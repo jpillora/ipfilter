a3bb19f1ff3faa648af4cb353fd1c576b5e67858
Ruxton <ruxton@rhythmandpoetry.net>
2017-06-30 11:37:14 +0800

    Remove excess functions in interface

--- ipfilter.go
+++ ipfilter.go
@@ -55,21 +55,7 @@ type Options struct {
 	BlockByDefault bool
 
 	Logger interface {
-		Fatal(v ...interface{})
-		Fatalf(format string, v ...interface{})
-		Fatalln(v ...interface{})
-		Flags() int
-		Output(calldepth int, s string) error
-		Panic(v ...interface{})
-		Panicf(format string, v ...interface{})
-		Panicln(v ...interface{})
-		Prefix() string
-		Print(v ...interface{})
 		Printf(format string, v ...interface{})
-		Println(v ...interface{})
-		SetFlags(flag int)
-		SetOutput(w io.Writer)
-		SetPrefix(prefix string)
 	}
 }
 
